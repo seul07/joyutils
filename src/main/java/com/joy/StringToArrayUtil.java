361dc77f1d411839176848a5ae2b124b5a5ef286
seul07 <sseul0701@gmail.com>
2022-04-14 14:42:13 +0900

    List to array

--- src/main/java/com/joy/StringToArrayUtil.java
+++ src/main/java/com/joy/StringToArrayUtil.java
@@ -9,12 +9,15 @@ public class StringToArrayUtil {
      * parse String to Array and return the array
      * if it occurs Exception then return null
      */
-    public static List<Integer> parseArray(String args) {
-        List<Integer> result = new ArrayList<>();
+    public static int[] parseArray(String args) {
+        int[] result = new int[0];
+//        List<Integer> result = new ArrayList<>();
         try {
             args = args.replaceAll(" ","");
+            result = new int[args.length()];
             for(int i=0; i<args.length();i++){
-                result.add(Integer.parseInt(args.charAt(i)+"") );
+                result[i] = Integer.parseInt(args.charAt(i)+"");
+//                result.add(Integer.parseInt(args.charAt(i)+"") );
             }
         }catch (Exception e){
             result = null;
