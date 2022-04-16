412940e9899e21ef771e4818eaf8285d696b15ac
shane <psh40963@naver.com>
2022-04-16 11:38:18 +0900

    fixed bug: couldn't parse Integer bigger than 2 digits

--- src/test/java/com/joy/StringToArrayUtilTest.java
+++ src/test/java/com/joy/StringToArrayUtilTest.java
@@ -1,14 +1,19 @@
 package com.joy;
 
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class StringToArrayUtilTest {
 
     @Test
-    void parseArray(){
-
+    void parseArray() {
+        assertThat(StringToArrayUtil.parseArray("1 2 1 3 1 1 2")).containsExactly(1, 2, 1, 3, 1, 1, 2);
+        assertThat(StringToArrayUtil.parseArray("1 2 1 3 1 1 2 ")).containsExactly(1, 2, 1, 3, 1, 1, 2);
+        assertThat(StringToArrayUtil.parseArray(" 1 2 1 3 1 1 2 ")).containsExactly(1, 2, 1, 3, 1, 1, 2);
+        assertThat(StringToArrayUtil.parseArray("11 12 123")).containsExactly(11, 12, 123);
+        assertThat(StringToArrayUtil.parseArray("11 12 abc123")).isNull();
     }
 
 
